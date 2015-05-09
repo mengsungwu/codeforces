67c4c91101b6390c07c3873a475a9d9ebd4e01c6
Meng-Sung Wu <mengsungwu@fortunewhite.org>
2015-05-09 16:15:25 +0800

    359B - Permutation

---
+++ src/359/b.py
@@ -0,0 +1,14 @@
+
+def main():
+    n, k = [int(x) for x in input().split()]
+    for i in range(n):
+        if k > 0:
+            print(2*i + 1, 2*i + 2, end=' ')
+            k -= 1
+        else:
+            print(2*i + 2, 2*i + 1, end=' ')
+
+
+
+if __name__ == '__main__':
+    main()
