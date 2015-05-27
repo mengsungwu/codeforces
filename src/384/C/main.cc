bf9d5c6a117fded9d088b9acf563a7ef800639aa
Meng-Sung Wu <mengsungwu@fortunewhite.org>
2015-05-27 21:05:01 +0800

    384C - Milking cows

--- src/384/C/main.cc
+++ src/384/C/main.cc
@@ -11,27 +11,31 @@ typedef long long int ll;
 
 std::vector<ll> a;
 
-ll s;
-std::vector<ll> d;
-
-class IntervalSumTree {
+typedef class SegmentTree {
 public:
-	
 
-	IntervalSumTree(std::vector<ll> data) {
+	SegmentTree(std::vector<ll> data) {
+
 		for (M = 1; M < data.size() + 2; M <<= 1)
 			;
 
 		tree.resize(M << 1);
 
-		for (ll i = M; i < data.size(); i++)
+		for (ll i = 0; i < data.size(); i++)
 			tree[M + i] = data[i];
-		for (ll i = M - 1; i > 0; i--)
+
+		for (ll i = M - 1; i > 0; i--) 
 			tree[i] = tree[i << 1] + tree[(i << 1) + 1];
+
 	}
 
 	ll query(ll x, ll y) {
-
+		ll total = 0;
+		for (x += M - 1, y += M + 1; x^y ^ 1; x >>= 1, y >>= 1) {
+			if (~x & 1) total += tree[x ^ 1];
+			if (y & 1) total += tree[y ^ 1];
+		}
+		return total;
 	}
 
 
@@ -39,7 +43,7 @@ private:
 	ll M;
 	std::vector<ll> tree;
 
-};
+} SegmentTree;
 
 
 int main(int argc, char *argv[])
@@ -47,17 +51,22 @@ int main(int argc, char *argv[])
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	
-	ll n;
+	ll n, total = 0;
 	std::cin >> n;
 	a.resize(n + 1);
 	for (ll i = 1; i <= n; i++) {
 		std::cin >> a[i];
 	}
 
+	SegmentTree tree(a);
 
-	ll x, y;
-	std::cin >> x >> y;
-	std::cout << query(x, y) << std::endl;
+	for (ll i = 1; i <= n; i++) {
+		if (a[i] == 0) {
+			total += tree.query(1, i);
+		}
+	}
+
+	std::cout << total << std::endl;
 
 	return 0;
 }
